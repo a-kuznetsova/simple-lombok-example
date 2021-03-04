d23534ac89aab3cd4da546962466777ec794089a
anastasia.kuznetsova <anastasia.kuznetsova@jetbrains.com>
2021-03-04 13:49:54 +0100

    Init repo

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat kitty = Cat
+                .builder()
+                .name("Leo")
+                .breed("British Shorthair")
+                .build();
+        System.out.println(kitty);
+    }
+}
