5ec1135af60defa4f96d708496782c706ea5b6cd
a-kuznetsova <anastasia.kuznetsova@jetbrains.com>
2021-03-09 10:37:39 +0100

    Added problems for testing inspections

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,12 +2,16 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception, Exception {
         Cat kitty = Cat
                 .builder()
                 .name("Leo")
                 .breed("British Shorthair")
                 .build();
+	if(true) System.out.println("true kitty");
+	Integer a = new Integer(2);
+	Integer b = new Integer(3);
+	if (a == b ) System.out.println("true kitty");
         System.out.println(kitty);
     }
 }
